b8686fff07705f075b0910b45a21647fe6515e58
Hyeonji-Jung <welloff.jj@gmail.com>
2021-08-23 19:10:37 +0900

    profile

--- rush00/src/pages/ProfilePage.js
+++ rush00/src/pages/ProfilePage.js
@@ -1,13 +1,33 @@
 import React from "react";
+import styled from "styled-components";
+
+const ProfilePageStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  h2 {
+    font-size: 21px;
+    font-weight: 600;
+    text-align: left;
+  }
+`;
 
 function ProfilePage() {
-  const id = "rush00";
-  const getUser = () => {
-    fetch(`http://localhost:4242/profile/${id}`)
-      .then((res) => res.text())
-      .then((res) => console.log("Profile Page"));
-  };
-  return <div></div>;
+  const email = "rush00@student.42seoul.kr";
+  const nickname = "rush00";
+
+  // const getUser = () => {
+  // };
+
+  return (
+    <ProfilePageStyled>
+      <h2>{email}의 프로필</h2>
+      <ul>
+        <li key="email">Email: {email}</li>
+        <li key="nickname">Nickname: {nickname}</li>
+      </ul>
+    </ProfilePageStyled>
+  );
 }
 
 export default ProfilePage;
