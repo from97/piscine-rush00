572a2c3a47b237120c1abb4805919549b67352ea
from97 <skm1104@daum.net>
2021-08-23 21:49:48 +0900

    ProfilePage 스타일링

--- rush00/src/pages/ProfilePage.js
+++ rush00/src/pages/ProfilePage.js
@@ -2,13 +2,31 @@ import React from "react";
 import styled from "styled-components";
 
 const ProfilePageStyled = styled.div`
+  width: 500px;
   display: flex;
   flex-direction: column;
   align-items: center;
+  letter-spacing: 0.05em;
+  border: 1px dashed #ddd;
+  margin: 90px auto;
+  padding: 30px 0 60px 0;
   h2 {
     font-size: 21px;
     font-weight: 600;
     text-align: left;
+    letter-spacing: 0.1em;
+    margin-bottom: 45px;
+  }
+  li {
+    line-height: 35px;
+  }
+  .username {
+    color: #00779b;
+    margin-right: 2px;
+  }
+  .profile_field {
+    font-weight: 500;
+    margin-right: 5px;
   }
 `;
 
@@ -21,10 +39,18 @@ function ProfilePage() {
 
   return (
     <ProfilePageStyled>
-      <h2>{email}의 프로필</h2>
+      <h2>
+        <span className="username">{nickname}</span>의 프로필
+      </h2>
       <ul>
-        <li key="email">Email: {email}</li>
-        <li key="nickname">Nickname: {nickname}</li>
+        <li key="nickname">
+          <span className="profile_field">Nickname: </span>
+          {nickname}
+        </li>
+        <li key="email">
+          <span className="profile_field">Email: </span>
+          {email}
+        </li>
       </ul>
     </ProfilePageStyled>
   );
