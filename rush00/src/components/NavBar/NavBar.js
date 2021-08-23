4f6f140a56da5a96e176c9625fdbd707951f1286
from97 <skm1104@daum.net>
2021-08-23 20:27:28 +0900

    NavBar active 효과 적용

--- rush00/src/components/NavBar/NavBar.js
+++ rush00/src/components/NavBar/NavBar.js
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import NavBarStyled from "./NavBar.styles";
 import { Link } from "react-router-dom";
 
 function NavBar() {
+  const [mode, setMode] = useState("home");
+
   return (
     <NavBarStyled>
-      <Link to="/">HOME</Link>
-      <Link to="/profile">PROFILE</Link>
-      <Link to="/login">LOGIN</Link>
-      <Link to="/board">BOARD</Link>
+      <NavBarStyled.LinkWrapper
+        onClick={() => setMode("home")}
+        isActive={mode === "home"}
+      >
+        <Link to="/">HOME</Link>
+      </NavBarStyled.LinkWrapper>
+      <NavBarStyled.LinkWrapper
+        onClick={() => setMode("profile")}
+        isActive={mode === "profile"}
+      >
+        <Link to="/profile">PROFILE</Link>
+      </NavBarStyled.LinkWrapper>
+      <NavBarStyled.LinkWrapper
+        onClick={() => setMode("login")}
+        isActive={mode === "login"}
+      >
+        <Link to="/login">LOGIN</Link>
+      </NavBarStyled.LinkWrapper>
+      <NavBarStyled.LinkWrapper
+        onClick={() => setMode("board")}
+        isActive={mode === "board"}
+      >
+        <Link to="/board">BOARD</Link>
+      </NavBarStyled.LinkWrapper>
     </NavBarStyled>
   );
 }
--- rush00/src/components/NavBar/NavBar.styles.js
+++ rush00/src/components/NavBar/NavBar.styles.js
@@ -7,14 +7,21 @@ const NavBarStyled = styled.nav`
   background-color: #eee;
   margin: 10px 20px;
   border-radius: 5px;
+  color: inherit;
 
   a {
-    margin: 10px 20px;
-
     :hover {
       color: #036581;
     }
   }
+`;
+
+NavBarStyled.LinkWrapper = styled.div`
+  width: 70px;
+  text-align: center;
+  padding: 10px 15px;
+  background-color: ${({ isActive }) => (isActive ? "#cee9ff" : "#eee")};
+  color: ${({ isActive }) => (isActive ? "#fff" : "#343a3f")};
 `;
 
 export default NavBarStyled;
