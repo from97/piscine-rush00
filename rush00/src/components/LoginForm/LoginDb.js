9c69aa62a1ad93330a85b10ef38ed9bae462f205
Hyeonji-Jung <welloff.jj@gmail.com>
2021-08-24 16:45:43 +0900

    login

---
+++ rush00/src/components/LoginForm/LoginDb.js
@@ -0,0 +1,17 @@
+const LoginDb = async (props) => {
+    const [email, passwd, nickname] = props;
+    console.log(props);
+    fetch("http://localhost:4242/auth/login", {
+      method: "POST",
+      headers: {
+        username: nickname,
+        password: passwd,
+        email: email,
+      },
+    })
+      .then((response) => console.log(response))
+      .catch((e) => console.log(e));
+  };
+  
+  export default LoingDb;
+  
