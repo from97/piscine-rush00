6481743e9cd98c2ab1570fc652deb28814c601ce
from97 <skm1104@daum.net>
2021-08-24 17:44:00 +0900

    .

--- rush00/src/components/Article/ArticleList.js
+++ rush00/src/components/Article/ArticleList.js
@@ -8,7 +8,7 @@ function ArticleList({ handleClick }) {
   useEffect(() => {
     const fetchList = async () => {
       try {
-        const response = await axios.get("http://localhost:4242/board");
+        const response = await axios.get("http://localhost:4242/board?idx=1");
         console.log("list", response);
       } catch (e) {
         console.log(e);
